24b7aa16c678f2df5d0fb5d5e20d86cc4d5438e6
Davide Pasetto <dpasetto69@gmail.com>
2016-11-01 10:38:53 -0400

    Added pre-save hook

--- core/src/main/web/app/helpers/helper.js
+++ core/src/main/web/app/helpers/helper.js
@@ -1212,6 +1212,11 @@
           if (_.isObject(evaluator)) delete evaluator.shellID;
         });
 
+        // apply hooks
+        if (window.beakerRegister !== undefined && window.beakerRegister.hooks !== undefined && window.beakerRegister.hooks.preSave !== undefined) {
+          notebookModelCopy = window.beakerRegister.hooks.preSave(notebookModelCopy);
+        }
+        
         // generate pretty JSON
         var prettyJson = bkUtils.toPrettyJson(notebookModelCopy);
         return prettyJson;
