1526404ac6e56ceb3a29e93c950a740341c0974c
Konstantin Tarletski <konstantin.tarletski@ignite.ee>
2016-11-14 17:20:29 +0200

    issue #4681 "begin to replace tables" -- Table resized if page resized, supporting of DateTime

--- core/src/main/web/outputdisplay/bko-table-ag-grid/bko-table.js
+++ core/src/main/web/outputdisplay/bko-table-ag-grid/bko-table.js
@@ -23,6 +23,16 @@
     function(
       $interval, $compile, $rootScope, bkEvaluateJobManager, bkUtils, bkNotificationService,
       bkOutputDisplayFactory, bkSparkContextManager) {
+    
+    var TIME_UNIT_FORMATS = {
+        DATETIME:     { title: 'datetime', format: 'YYYY-MM-DD HH:mm:ss.SSS ZZ' },
+        DAYS:         { title: 'date', format: 'YYYY-MM-DD' },
+        HOURS:        { title: 'hours', format: 'YYYY-MM-DD HH:mm ZZ' },
+        MINUTES:      { title: 'minutes', format: 'HH:mm ZZ' },
+        SECONDS:      { title: 'seconds', format: 'HH:mm:ss ZZ' },
+        MILLISECONDS: { title: 'milliseconds', format: 'HH:mm:ss.SSS ZZ' }
+      };
+    
     return {
       template: JST['bko-table-ag-grid/bko-table'],
       require: '^bkOutputDisplay',
@@ -47,7 +57,7 @@
         $scope.getRowCountToShow = function(){
           return 20;//TODO replace with user menu 
         }
-        
+
         $scope.getActualRowCountToShow = function(){
           var rowCount = $scope.getRowCountToShow();
           if($scope.values.length < rowCount){
@@ -71,12 +81,21 @@
         var convertColumns = function(input){
           var ret = [];
           for(var i = 0; i < input.length; i++){
-            ret.push({headerName: '' + input[i], field: '' + input[i]});
+            var retItem = {
+                headerName: '' + input[i],
+                field: '' + input[i],
+            };
+            if($scope.types[i] == 'datetime'){
+              retItem.cellRenderer = function (params) {
+                return bkUtils.formatTimestamp(params.value.timestamp, null, TIME_UNIT_FORMATS.DATETIME.format);
+              }
+            }
+            ret.push(retItem);
           }
           return ret;
         }
         
-        var fitTableToCollumns = function(){
+        var setTableWidth = function(){
           setTimeout(function(){//wait for document ready
             
             var width = 0;
@@ -84,13 +103,13 @@
               width += col.actualWidth;
             });
 
-            //if($scope.getActualRowCountToShow() > $scope.getRowCountToShow()){
             width += getVerticalScrollBarWidth();
-
             var tableParentDiv = document.getElementById($scope.id_parent);
             
             if(tableParentDiv.offsetWidth >= width){
               document.getElementById($scope.id).style.width = width + 'px';
+            }else{
+              document.getElementById($scope.id).style.width = tableParentDiv.offsetWidth + 'px';
             }
           }, 0);
         }
@@ -101,6 +120,12 @@
               allColumnIds.push(columnDef);
           });
           $scope.gridOptions.columnApi.autoSizeColumns(allColumnIds);
+          setTableHeight();
+        }
+        
+        var onPageResized = function(){
+          setTableHeight();
+          setTableWidth();
         }
         
         $scope.agClass = function (){
@@ -115,21 +140,24 @@
           }
           return ret;
         }
+        
+        var setTableHeight = function(){
+          setTimeout(function(){//wait for document ready
+            var width = 0;
+            $scope.gridOptions.columnApi.getAllDisplayedColumns().forEach( function(col) {
+              width += col.actualWidth;
+            });
 
-        $scope.getTableHeight = function(){
-          var ret = $scope.getActualRowCountToShow() * getRowHeight() + getHeaderHeight() + 2 + 3; //borders
-
-/*          var width = 0;
-          $scope.gridOptions.columnApi.getAllDisplayedColumns().forEach( function(col) {
-            width += col.actualWidth;
-          });
-          width += getVerticalScrollBarWidth();
-          var tableParentDiv = document.getElementById($scope.id_parent);
-          
-          if(tableParentDiv.offsetWidth < width){
-            ret += getHorizantalScrollBarWidth();
-          }*/
-          return ret;
+            width += getVerticalScrollBarWidth();
+            var tableParentDiv = document.getElementById($scope.id_parent);
+            
+            var ret = $scope.getActualRowCountToShow() * getRowHeight() + getHeaderHeight() + 2 + 3; //borders
+            if(tableParentDiv.offsetWidth < width){
+              ret +=10;
+            }
+            document.getElementById($scope.id).style.height = ret + 'px';
+            
+          }, 0);
         }
 
         $scope.init = function() {
@@ -154,12 +182,15 @@
               enableSorting: true,
               columnDefs: $scope.convertedColumns,
               rowData: $scope.convertedRows,
-              onColumnResized: fitTableToCollumns,
+              onColumnResized: setTableWidth,
               onGridReady: onGridReady,
-          };      
-
+          };
+          
+          $(window).bind('resize.' + $scope.id, function () {
+            onPageResized();
+          });
         }
- 
+
         $scope.init();
 
       },
